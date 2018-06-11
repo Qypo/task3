a270ab08b688d2c9cc23642149da445e88a300f4
Aleksey <33744877+Qypo@users.noreply.github.com>
2018-06-11 14:21:30 +0300

    v2

--- task1.js
+++ task1.js
@@ -1,21 +1,23 @@
-if (process.argv.length <= 2) {
-    process.exit(-1);
-}
+// if (process.argv.length <= 2) {
+//     process.exit(-1);
+// }
 
 var i = process.argv[2], a = process.argv[3], b = process.argv[4];
 
-if (i < 0 || i > 10000 || a < 2 || a > 36 || b < 2 || b > 36)
-  process.exit(-1);
+if (i < 0 || i > 10000 || a < 2 || a > 36 || b < 2 || b > 36) {
+    process.exit(-1);
+}
 
 function perevod(i, a ,b)
 {
+
     var str = i + ''; //преобразуем в строку
     var arr = str.split(''); //делим в массив
 
-    for (var k = 0; k <= arr.length; k++)
-    {
-         if (arr[k] >= a)                          //проверяем, чтобы цифры числа не превышали основание
-             process.exit(-1);
+    for (var k = 0; k < arr.length - 1; k++) {
+        if (arr[k] >= a) {          //проверяем, чтобы цифры числа не превышали основание
+            process.exit(-1);
+        }
     }
 
     var des = parseInt(i, a); //перевод в десятичную
@@ -25,3 +27,5 @@ function perevod(i, a ,b)
 }
 
 process.stdout.write(perevod(i, a ,b));
+
+//не работает с дробными
