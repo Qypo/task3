9cc8dccf0addda3f6cba784f7e3d56b4b8e0c771
Aleksey <33744877+Qypo@users.noreply.github.com>
2018-06-11 15:33:39 +0300

    v2

--- task1.js
+++ task1.js
@@ -3,7 +3,7 @@
 // }
 
 var i = process.argv[2], a = process.argv[3], b = process.argv[4];
-
+//console.log(i, a, b);
 if (i < 0 || i > 10000 || a < 2 || a > 36 || b < 2 || b > 36) {
     process.exit(-1);
 }
@@ -11,14 +11,16 @@ if (i < 0 || i > 10000 || a < 2 || a > 36 || b < 2 || b > 36) {
 function perevod(i, a ,b)
 {
 
-    var str = i + ''; //преобразуем в строку
-    var arr = str.split(''); //делим в массив
-
-    for (var k = 0; k < arr.length - 1; k++) {
-        if (arr[k] >= a) {          //проверяем, чтобы цифры числа не превышали основание
-            process.exit(-1);
-        }
-    }
+    // var str = i + ''; //преобразуем в строку
+    // var arr = str.split(''); //делим в массив
+    // console.log(i, a, b);
+    // for (var k = 0; k < arr.length; k++) {
+    //     //console.log(arr[k]);
+    //     if (arr[k] >= a) {          //проверяем, чтобы цифры числа не превышали основание
+    //          //console.log(arr[k]);
+    //         //process.exit(-1);
+    //     }
+    // }
 
     var des = parseInt(i, a); //перевод в десятичную
     var num = des.toString(b); //из десятичной в любую другую
@@ -29,3 +31,4 @@ function perevod(i, a ,b)
 process.stdout.write(perevod(i, a ,b));
 
 //не работает с дробными
+//закоменчена проверка на правильность записи
--- task3.js
+++ task3.js
@@ -6,22 +6,14 @@ var a = process.argv[2], b = process.argv[3];
 
 if (a < 1 || a >= 10000 || b < 1 || b >= 10000)
   process.exit(-1);
+// console.log(a, b);
 
-function nod(a, b)
-{
-  if (a == 0)
-  {
-    return b;
-  }
-  
-  while (b != 0) 
-  {
-    if (a > b)
-         a = a - b;
-    else
-         b = b - a;
-  }
-  return a;
-}
+var nod = function(a, b) {
+    if ( ! b) {
+        return a;
+    }
+
+    return nod(b, a % b);
+};
 
 process.stdout.write(nod(a, b));
