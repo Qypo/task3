78b505300c59a05d2b43335978e4b720d52c28eb
Aleksey <33744877+Qypo@users.noreply.github.com>
2018-06-11 16:14:38 +0300

    v2

--- task3.js
+++ task3.js
@@ -4,16 +4,20 @@ if (process.argv.length <= 2) {
  
 var a = process.argv[2], b = process.argv[3];
 
+a = Math.abs(a);
+b = Math.abs(b);
+
 if (a < 1 || a >= 10000 || b < 1 || b >= 10000)
   process.exit(-1);
 // console.log(a, b);
 
 var nod = function(a, b) {
-    if ( ! b) {
+    if ( b === 0) {
         return a;
     }
 
     return nod(b, a % b);
 };
+var str = nod(a, b) + '';
 
-process.stdout.write(nod(a, b));
+process.stdout.write(str);
